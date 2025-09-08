48e15a2d60c3c9bac46dcb3f2921d5473e92b5f8
Gustavo Luiz Walker <gustavolwalker@gmail.com>
2025-09-08 20:35:49 -0300

    feat: start app

---
+++ app/src/main/java/br/com/example/the_worstest_films/TheWorstestFilmsApplication.java
@@ -0,0 +1,13 @@
+package br.com.example.the_worstest_films;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheWorstestFilmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheWorstestFilmsApplication.class, args);
+	}
+
+}
