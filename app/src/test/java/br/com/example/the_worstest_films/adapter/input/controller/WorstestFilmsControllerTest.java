5b183135918664c0911cb541aca52c019d54a3a2
Gustavo Luiz Walker <gustavolwalker@gmail.com>
2025-09-15 20:03:51 -0300

    test: add test to verify values of default file

--- app/src/test/java/br/com/example/the_worstest_films/adapter/input/controller/WorstestFilmsControllerTest.java
+++ app/src/test/java/br/com/example/the_worstest_films/adapter/input/controller/WorstestFilmsControllerTest.java
@@ -36,6 +36,32 @@ public class WorstestFilmsControllerTest {
     }
 
     @Test
+    public void shouldReturnTheWorstestFilmsByDefaultFile() throws Exception {
+        mockMvc.perform(get("/worstest-films/v1/producers"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.min").isArray())
+                .andExpect(jsonPath("$.max").isArray())
+                .andExpect(jsonPath("$.min[0]").exists())
+                .andExpect(jsonPath("$.min[0].producer").exists())
+                .andExpect(jsonPath("$.min[0].producer").value("Joel Silver"))
+                .andExpect(jsonPath("$.min[0].interval").exists())
+                .andExpect(jsonPath("$.min[0].interval").value(1))
+                .andExpect(jsonPath("$.min[0].previousWin").exists())
+                .andExpect(jsonPath("$.min[0].previousWin").value(1990))
+                .andExpect(jsonPath("$.min[0].followingWin").exists())
+                .andExpect(jsonPath("$.min[0].followingWin").value(1991))
+                .andExpect(jsonPath("$.max[0]").exists())
+                .andExpect(jsonPath("$.max[0].producer").exists())
+                .andExpect(jsonPath("$.max[0].producer").value("Matthew Vaughn"))
+                .andExpect(jsonPath("$.max[0].interval").exists())
+                .andExpect(jsonPath("$.max[0].interval").value(13))
+                .andExpect(jsonPath("$.max[0].previousWin").exists())
+                .andExpect(jsonPath("$.max[0].previousWin").value(2002))
+                .andExpect(jsonPath("$.max[0].followingWin").exists())
+                .andExpect(jsonPath("$.max[0].followingWin").value(2015));
+    }
+
+    @Test
     public void shouldReturnNotFound() throws Exception {
         mockMvc.perform(get("/worstest-films/producers"))
                 .andExpect(status().isNotFound());
